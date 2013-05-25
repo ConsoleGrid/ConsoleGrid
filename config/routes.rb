181ed093c6d803cdb9645e71f51c0b184215f3fa
Scott Rice <meris608@gmail.com>
2013-05-25 13:05:51 -0700

    God I'm an idiot...

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Steamgrid::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'games#find', :as => :homepage_search
+  root :to => 'games#find'
 
   # See how all your routes lay out with "rake routes"
 
