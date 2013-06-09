7867610704788ba21ee450a032cb4f487212a0c7
Scott Rice <meris608@gmail.com>
2013-06-08 17:48:27 -0700

    Customizes Admin Interface

--- app/admin/games.rb
+++ app/admin/games.rb
@@ -1,5 +1,13 @@
 ActiveAdmin.register Game do
-  # index do
-  #     
-  #   end
+  index do
+    id_column
+    column "Name", :sortable => :name do |game|
+      link_to game.name, admin_game_path(game)
+    end
+    column "Console" do |game|
+      game.console.name
+    end
+    column :created_at
+    default_actions
+  end
 end
--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Picture do
+  filter :game_name, :as => :string
+  filter :user, :as => :select
+  filter :created_at, :as => :date_range
   
+  index :as => :grid do |picture|
+    a truncate(picture.game.name,:length => 38), :href => admin_game_path(picture.game)
+    div do
+      a :href => admin_picture_path(picture) do
+        image_tag picture.image_url, :width => 214, :height => 100
+      end
+    end
+    a "by " + picture.user.email, :href => admin_user_path(picture.user)
+  end
 end
