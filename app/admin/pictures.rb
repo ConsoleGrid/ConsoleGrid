d95ca39d44aed14a22dbfc41b3a5c51d2d4bcb88
Scott Rice <meris608@gmail.com>
2013-11-19 12:51:01 -0600

    Adds preview of image in picture admin page.
    Fixes #13

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -12,4 +12,20 @@ ActiveAdmin.register Picture do
     end
     a "by " + picture.user.email, :href => admin_user_path(picture.user)
   end
+
+
+  show do |picture|
+    attributes_table do
+      row :image do |picture|
+        image_tag picture.image_url
+      end
+      row :id
+      row :game
+      row :user
+      row :image_url
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
 end
