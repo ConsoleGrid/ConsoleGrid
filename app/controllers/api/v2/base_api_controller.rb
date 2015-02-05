5cf1645339c7bf4a69f627c51d46421bb8041c26
Scott Rice <srice@fb.com>
2015-02-05 12:38:06 -0700

    Adds CORS header to API V2.

--- app/controllers/api/v2/base_api_controller.rb
+++ app/controllers/api/v2/base_api_controller.rb
@@ -6,6 +6,8 @@ class Api::V2::BaseApiController < ApplicationController
   # would overwrite this one
   rescue_from Exceptions::APIError, :with => :api_error_response
 
+  before_filter :set_cors_headers
+
   def error_response(message)
     render :json => { :error => message}
   end
@@ -16,5 +18,15 @@ class Api::V2::BaseApiController < ApplicationController
 
   def exception_error_response(exception)
     error_response exception.message
+  end
+
+  def set_cors_headers
+    # When reading online it was recommended to not set Allow-Credentials to
+    # true if you are using a wildcard Allow-Origin. Since I want to support
+    # user-specific features via the API I need to restrict the Allow-Origin
+    # value to just the site that originally made the request
+    return unless request.headers["HTTP_ORIGIN"]
+    headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+    headers['Access-Control-Allow-Credentials'] = 'true'
   end
 end
