36146b83dba453dc2758872b4af54175ab112d28
Scott Rice <srice@fb.com>
2015-02-05 12:33:23 -0700

    Adds CORS header to API V1. Fixes #28

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::ApiController < ApplicationController
+  before_filter :set_cors_headers
+
+  def set_cors_headers
+    # We don't need to authenticate for this API endpoint, so there is no
+    # point giving it the Allow-Credentials header. Since that is the case we
+    # don't need to be restrictive about who we allow requests from, and can
+    # just use '*'
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
+
   def top_picture
     console_id = Console.find_by_shortname(params[:console]).id
     @matched_game = Game.easy_search(params[:game], :console_id => console_id).first
