2950b4253460a253d2ba3083fe4ddb97121871eb
Scott Rice <srice@fb.com>
2016-01-03 15:12:37 -0800

    Add tests for v1 of the APIController
    I need to switch Ice to v2, but v1 needs to stick around for a while. As such, I need to add better tests for it.
    
    Luckily I added a bunch of nice fixtures when I did the tests for v2, so I can piggyback on those.

--- test/functional/api/v1/api_controller_test.rb
+++ test/functional/api/v1/api_controller_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class Api::V1::ApiControllerTest < ActionController::TestCase
-  test "should get top_picture" do
+  setup do
+    # Since we have a single endpoint for searching both consoles and games,
+    # we need to reindex them both before running a test
+    Console.reindex
+    Game.reindex
+  end
+
+  test "should return no content when missing game" do
+    # Zelda is in the fixtures, but only N64/Gamecube games
     get :top_picture, :console => "NES", :game => "Zelda"
-    assert_response :success
+    assert_response(204)
+  end
+
+  test "should return an image link when found a game" do
+    get :top_picture, :console => "N64", :game => "Ocarina"
+    assert_response(200)
+    assert_equal @response.body, pictures(:oot_picture_one).image_url
+  end
+
+  test "should return an empty string when game has no pictures" do
+    get :top_picture, :console => "N64", :game => "Majora"
+    assert_response(200)
+    assert_equal @response.body, ""
   end
 end
